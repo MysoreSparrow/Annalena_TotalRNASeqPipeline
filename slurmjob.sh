a308f1698a3f457862a4a637c9aac37a79dbca95
Keshava Prasad Gubbi <98920181+MysoreSparrow@users.noreply.github.com>
2022-08-04 14:22:07 +0200

    Add files via upload

--- slurmjob.sh
+++ slurmjob.sh
@@ -13,32 +13,37 @@ SBATCH --job-name=prodce_bams_totalrnaseq_annalena
 SBATCH --time=24:00:00 
 
 #Declare the merged STDOUT/STDERR file
-SBATCH --output=totalrnaseq_annalena_output.%J.out
+SBATCH --output=/data/keshav/total_rnaseq/output/totalrnaseq_annalena_output.%J.out
 
 #Run something
 #module load R
 #echo $PATH #only diplays the current PATH-variable
 
 #Please note: you have to load the modules and every other variable you might have set in yout .bashrc again in the execution file, in order for it to be loaded properly on the compute nodes.
-
-source /home/keshav/anaconda3/envs/kpENV
-conda activate kpENV
+pwd
+conda info --envs
+source /home/keshav/anaconda3/bin/activate
+conda activate base 
 
 echo "Starting with Genome Variable."
 # define variables
 gd=/data/keshav/total_rnaseq/genome_index_fromSTAR_usingm39
 echo $gd
-filepath=/data/keshav/total_rnaseq/Annalena/merged
+filepath=/data/keshav/total_rnaseq/Annalena/merged_files
 
 
 # get the data files as a list of comma separated R1 files and then with a space for R2 files.
-R1FILES=$filepath/AJ1_sm_R1.fastq.gz,$filepath/AJ2_sm_R1.fastq.gz,$filepath/AJ3_sm_R1.fastq.gz,$filepath/AJ4_sm_R1.fastq.gz,$filepath/AJ5_sm_R1.fastq.gz,$filepath/AJ6_sm_R1.fastq.gz,$filepath/AJ7_sm_R1.fastq.gz,$filepath/AJ8_sm_R1.fastq.gz,$filepath/AJ9_sm_R1.fastq.gz,$filepath/AJ10_sm_R1.fastq.gz,$filepath/AJ11_sm_R1.fastq.gz,$filepath/AJ12_sm_R1.fastq.gz,$filepath/AJ13_sm_R1.fastq.gz,$filepath/AJ14_sm_R1.fastq.gz,$filepath/AJ15_sm_R1.fastq.gz,$filepath/AJ16_sm_R1.fastq.gz,$filepath/AJ17_sm_R1.fastq.gz,$filepath/AJ18_sm_R1.fastq.gz,$filepath/AJ19_sm_R1.fastq.gz,$filepath/AJ20_sm_R1.fastq.gz
-R2FILES=filepath/AJ1_sm_R2.fastq.gz,$filepath/AJ2_sm_R2.fastq.gz,$filepath/AJ3_sm_R2.fastq.gz,$filepath/AJ4_sm_R2.fastq.gz,$filepath/AJ5_sm_R2.fastq.gz,$filepath/AJ6_sm_R2.fastq.gz,$filepath/AJ7_sm_R2.fastq.gz,$filepath/AJ8_sm_R2.fastq.gz,$filepath/AJ9_sm_R2.fastq.gz,$filepath/AJ10_sm_R2.fastq.gz,$filepath/AJ11_sm_R2.fastq.gz,$filepath/AJ12_sm_R2.fastq.gz,$filepath/AJ13_sm_R2.fastq.gz,$filepath/AJ14_sm_R2.fastq.gz,$filepath/AJ15_sm_R2.fastq.gz,$filepath/AJ16_sm_R2.fastq.gz,$filepath/AJ17_sm_R2.fastq.gz,$filepath/AJ18_sm_R2.fastq.gz,$filepath/AJ19_sm_R2.fastq.gz,$filepath/AJ20_sm_R2.fastq.gz
+R1FILES=$filepath/AJ1_sm_R1.fastq.gz,$filepath/AJ2_sm_R1.fastq.gz,$filepath/AJ3_sm_R1.fastq.gz,$filepath/AJ4_sm_R1.fastq.gz,$filepath/AJ5_sm_R1.fastq.gz,$filepath/AJ6_sm_R1.fastq.gz,$filepath/AJ7_sm_R1.fast$
+R2FILES=$filepath/AJ1_sm_R2.fastq.gz,$filepath/AJ2_sm_R2.fastq.gz,$filepath/AJ3_sm_R2.fastq.gz,$filepath/AJ4_sm_R2.fastq.gz,$filepath/AJ5_sm_R2.fastq.gz,$filepath/AJ6_sm_R2.fastq.gz,$filepath/AJ7_sm_R2.fast$
 
 
 #for f in $(ls *.fastq.gz)
 #do
 #    echo $f
+#/data/keshav/total_rnaseq/Annalena/merged_files/aligned
 STAR --runThreadN 8 --genomeDir $gd --readFilesIn $R1FILES $R2FILES --outSAMtype BAM SortedByCoordinate --quantMode GeneCounts --outFileNamePrefix ./aligned/aligned_ --readFilesCommand zcat
 #done
 echo "done!"
+
+
+
